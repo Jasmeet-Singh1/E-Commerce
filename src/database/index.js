2751beb96dd8e1f4c571ba1aeba647f045ad1caf
Jasmeet-Singh1 <144895322+Jasmeet-Singh1@users.noreply.github.com>
2023-10-24 14:21:38 +0530

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectToDb = async () => {
-  const connectionUrl = 'mongodb+srv://SaveContacts:<>@cluster0.3wzilyu.mongodb.net/';
+   const connectionUrl = 'mongodb+srv://Ecommercy:<password>@ecommerce.ttapqkr.mongodb.net/';
 };
 
 export default connectToDb;
