4cf336e5c5f5b719a767b6bfa2040fc61bdc09da
Jasmeet-Singh1 <144895322+Jasmeet-Singh1@users.noreply.github.com>
2023-10-24 14:22:04 +0530

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,14 @@
 import mongoose from 'mongoose';
 
+const configOption = {
+  useNewUrlParser: true,
+};
 const connectToDb = async () => {
-   const connectionUrl = 'mongodb+srv://Ecommercy:<password>@ecommerce.ttapqkr.mongodb.net/';
+  const connectionUrl = 'mongodb+srv://Ecommercy:<password>@ecommerce.ttapqkr.mongodb.net/';
+
+  (await mongoose.connect(connectionUrl, configOption))
+    .then(() => console.log('Ecommerce database connected successfully'))
+    .catch((err) => console.log(`Getting Error from DB connection ${err.message}`));
 };
 
 export default connectToDb;
